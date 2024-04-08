cbedbcb8d42f18958cd72946b3d8facc3aa1dbad
Mansi Sehgal <mansisehgal22@gmail.com>
2024-04-08 15:06:13 +0530

    aded new models for like, tweet, comment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -185,8 +185,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     { new: true }
@@ -430,7 +430,7 @@ const getWatchHistory = asyncHandler(async (req, res) => {
   const user = await User.aggregate([
     {
       $match: {
-        _id: new mongoose.Types.ObjectId(req.user._id),
+        _id: new mongoose.Schema.Types.ObjectId(req.user._id),
       },
     },
     {
